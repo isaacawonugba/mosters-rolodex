036031c1d4afbf99da7ee732d783c4468d25c431
unknown <isaacawonugba@gmail.com>
2020-10-17 22:41:32 +0100

    Updates

---
+++ precache-manifest.8a1386d0f612e8582f491c4e956d22d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8955cff32cec030bc91f23582a1574ec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e5065a316a82097b0b2",
+    "url": "/monsters-rolodex/static/css/main.9abe7c14.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5065a316a82097b0b2",
+    "url": "/monsters-rolodex/static/js/main.d4270d40.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
